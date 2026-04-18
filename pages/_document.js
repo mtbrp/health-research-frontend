281894e161ad48db3fc9bb34065cfc7ec93ad906
mtbrp <bravom@cnu.edu.ph>
2026-04-18 12:33:27 +0800

    Update and rename _document.js to pages/_document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* This loads Tailwind CSS globally for the whole site */}
-        <script src="https://cdn.tailwindcss.com"></script>
-      </Head>
-      <body>
+      <Head />
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
