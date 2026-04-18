8ea7a11d39f5d89687dc602c07eb13636392f621
mtbrp <bravom@cnu.edu.ph>
2026-04-18 12:32:14 +0800

    Update and rename _app.js to pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import 'health-research-frontend/styles/globals.css' // This connects your CSS to the app
+import './globals.css' // Notice we removed one dot and the "styles/" folder
 import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Component {...pageProps} />
     </ThemeProvider>
   )
