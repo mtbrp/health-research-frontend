94d80caaa2ec06f12da0bf64f4b3eded591a24b8
mtbrp <bravom@cnu.edu.ph>
2026-04-18 12:45:11 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'health-research-frontend/styles/globals.css' // Notice we removed one dot and the "styles/" folder
+import './globals.css' // Notice we removed one dot and the "styles/" folder
 import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
